9e4d6c9dfe743bece0b1efc13da47b8253f22c2d
Paul Hamill <enquiries@nestboxproperty.co.uk>
2022-07-12 14:49:17 +0100

    classes defined

---
+++ models/album.py
@@ -0,0 +1,8 @@
+class Album:
+
+    def __init__(self, title, genre, artist=None, id=None):
+        self.title = title
+        self.genre = genre
+        self.artist = artist
+        self.id = id
+
